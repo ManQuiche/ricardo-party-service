c018bbaf688135c7dca212d60e467b09d38d17ff
Matéo RABOTIN <mateo@rabotin.net>
2022-03-26 16:58:43 +0100

    wip: using ricardo err in party controller

--- internal/driving/http/party/controller.go
+++ internal/driving/http/party/controller.go
@@ -3,12 +3,12 @@ package party
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	ricardoerr "gitlab.com/ricardo-public/errors/pkg/errors"
 	tokens "gitlab.com/ricardo-public/jwt-tools/pkg"
 	"gorm.io/gorm"
 	"net/http"
 	"ricardo/party-service/internal/core/app/party"
 	"ricardo/party-service/internal/core/entities"
-	errors2 "ricardo/party-service/pkg/errors"
 	"strconv"
 )
 
@@ -34,7 +34,7 @@ func (c controller) Create(gtx *gin.Context) {
 	var cpr entities.CreatePartyRequest
 	err := gtx.ShouldBindJSON(&cpr)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusBadRequest)
+		_ = ricardoerr.GinErrorHandler(gtx, ricardoerr.New(ricardoerr.ErrBadRequest, ""))
 		return
 	}
 
@@ -44,7 +44,7 @@ func (c controller) Create(gtx *gin.Context) {
 	}
 	cParty, err := c.service.Save(gtx.Request.Context(), p)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return
 	}
 
@@ -55,7 +55,7 @@ func (c controller) Update(gtx *gin.Context) {
 	var upr entities.UpdatePartyRequest
 	err := gtx.ShouldBindJSON(&upr)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusBadRequest)
+		_ = ricardoerr.GinErrorHandler(gtx, ricardoerr.New(ricardoerr.ErrBadRequest, ""))
 		return
 	}
 
@@ -74,7 +74,7 @@ func (c controller) Update(gtx *gin.Context) {
 
 	uParty, err := c.service.Save(gtx.Request.Context(), p)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return
 	}
 
@@ -84,7 +84,7 @@ func (c controller) Update(gtx *gin.Context) {
 func (c controller) Get(gtx *gin.Context) {
 	parties, err := c.service.GetAll(gtx.Request.Context())
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, ricardoerr.New(ricardoerr.ErrBadRequest, ""))
 		return
 	}
 
@@ -99,7 +99,7 @@ func (c controller) GetForUser(gtx *gin.Context) {
 
 	parties, err := c.service.GetAllForUser(gtx.Request.Context(), uint(userId))
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return
 	}
 
@@ -109,12 +109,13 @@ func (c controller) GetForUser(gtx *gin.Context) {
 func (c controller) GetOne(gtx *gin.Context) {
 	partyId, err := strconv.ParseUint(gtx.Param("party_id"), 10, 64)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusBadRequest)
+		_ = ricardoerr.GinErrorHandler(gtx, ricardoerr.New(ricardoerr.ErrBadRequest, ""))
+		return
 	}
 
 	parties, err := c.service.Get(gtx.Request.Context(), uint(partyId))
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return
 	}
 
@@ -125,7 +126,7 @@ func (c controller) Delete(gtx *gin.Context) {
 	var dpr entities.DeletePartyRequest
 	err := gtx.ShouldBindJSON(&dpr)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusBadRequest)
+		_ = ricardoerr.GinErrorHandler(gtx, ricardoerr.New(ricardoerr.ErrBadRequest, ""))
 		return
 	}
 
@@ -136,7 +137,7 @@ func (c controller) Delete(gtx *gin.Context) {
 
 	err = c.service.Delete(gtx.Request.Context(), dpr.ID)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return
 	}
 
@@ -146,13 +147,13 @@ func (c controller) Delete(gtx *gin.Context) {
 func (c controller) canUpdateOrDelete(gtx *gin.Context, partyID uint) (bool, error) {
 	p, err := c.service.Get(gtx.Request.Context(), partyID)
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusNotFound)
+		_ = ricardoerr.GinErrorHandler(gtx, ricardoerr.New(ricardoerr.ErrBadRequest, ""))
 		return false, err
 	}
 
 	strToken, err := tokens.ExtractTokenFromHeader(gtx.GetHeader(tokens.AuthorizationHeader))
 	if err != nil {
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusInternalServerError)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return false, err
 	}
 
@@ -162,7 +163,7 @@ func (c controller) canUpdateOrDelete(gtx *gin.Context, partyID uint) (bool, err
 
 	if uint(userID) != p.UserID && claims.Role != tokens.AdminRole {
 		err = errors.New("unauthorized to update or delete")
-		_ = errors2.GinErrorHandler(gtx, err, http.StatusUnauthorized)
+		_ = ricardoerr.GinErrorHandler(gtx, err)
 		return false, err
 	}
 
